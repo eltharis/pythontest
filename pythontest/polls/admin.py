33fb12b471da2ffd3a7392a9bbb17cf6ef932e5e
Wojciech Prokop <at4taerial@gmail.com>
2015-12-27 20:53:20 +0100

    tutorial models + admin panel integration

--- pythontest/polls/admin.py
+++ pythontest/polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choice
+
+
+@admin.register(Question)
+class AdminQuestion(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class AdminChoice(admin.ModelAdmin):
+    pass
+
+
