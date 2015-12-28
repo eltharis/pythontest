c4fe97b755f618be2ac2bdf379ff394fc7e099a8
Wojciech Prokop <at4taerial@gmail.com>
2015-12-28 16:50:11 +0100

    i18n fixes

--- registrations/apps.py
+++ registrations/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class RegistrationsConfig(AppConfig):
     name = 'registrations'
-    verbose_name = 'Zapisy'
+    verbose_name = _('registrations')
